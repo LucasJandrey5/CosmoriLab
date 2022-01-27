1ee979c50fc511c3493b248432bc7ca361811dda
Lucas Jandrey <lucasjandrey5@gmail.com>
2022-01-27 13:07:12 -0300

    Home, login and register page created

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,14 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
 Route::get('/userList', function () {
     return view('userListPage');
 });
